4bbc78216c1854805ded0418f457fa248ef2a83e
Micaela <micaela.cavallo@globant.com>
2015-02-10 23:52:07 -0300

    Pictures and Descriptions of games added.

---
+++ app/src/main/java/com/example/micaela/playstationstore/GameDescriptionFragment.java
@@ -0,0 +1,23 @@
+package com.example.micaela.playstationstore;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GameDescriptionFragment extends Fragment {
+
+    public GameDescriptionFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_game_description, container, false);
+        return rootView;
+    }
+}
